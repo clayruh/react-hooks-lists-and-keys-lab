ed56c73965d4448ec423de6387cf1248957c37a9
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-03 09:46:08 -0400

    using props to update website and understanding mapping

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+
+  const mapProjects = projects.map( projects => 
+  <ProjectItem key={projects.id} project={projects} name={projects.name} about={projects.about} technologies={projects.technologies}/> )
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {mapProjects}
+      </div>
     </div>
   );
 }
